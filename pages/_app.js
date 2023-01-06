92b6b130c6b69064f91e1871b647554545086f5e
Lee <71070167+lmduckworth@users.noreply.github.com>
2023-01-06 11:02:21 +0000

    All pages share header etc and <a href> no longer required

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,33 @@
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import Layout, { GradientBackground } from '../components/Layout';
+import SEO from '../components/SEO';
+import { getGlobalData } from '../utils/global-data';
+
 import '../styles/globals.css';
 import 'prismjs/themes/prism-tomorrow.css';
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
+  const globalData = getGlobalData();
+
+  return (    
+    <Layout>
+      <main className="w-full">
       <span className="theme-bejamas" />
+      <SEO title={globalData.name} description={globalData.blogTitle} />      
+      <Header name={globalData.name} />
       <Component {...pageProps} />
-    </>
+      </main>
+      <Footer copyrightText={globalData.footerText} />
+      <GradientBackground
+        variant="large"
+        className="fixed top-20 opacity-40 dark:opacity-60"
+      />
+      <GradientBackground
+        variant="small"
+        className="absolute bottom-0 opacity-20 dark:opacity-10"
+      />
+    </Layout>
   );
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,14 @@
 import Link from 'next/link';
 import { getPosts } from '../utils/mdx-utils';
 
-import Footer from '../components/Footer';
-import Header from '../components/Header';
-import Layout, { GradientBackground } from '../components/Layout';
+import SEO from '../components/SEO';
 import ArrowIcon from '../components/ArrowIcon';
 import { getGlobalData } from '../utils/global-data';
-import SEO from '../components/SEO';
 
 export default function Index({ posts, globalData }) {
   return (
-    <Layout>
+    <>      
       <SEO title={globalData.name} description={globalData.blogTitle} />
-      <Header name={globalData.name} />
-      <main className="w-full">
         <h1 className="text-3xl lg:text-5xl text-center mb-12">
           {globalData.blogTitle}
         </h1>
@@ -26,8 +21,8 @@ export default function Index({ posts, globalData }) {
               <Link
                 as={`/posts/${post.filePath.replace(/\.mdx?$/, '')}`}
                 href={`/posts/[slug]`}
+                className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4"
               >
-                <a className="py-6 lg:py-10 px-6 lg:px-16 block focus:outline-none focus:ring-4">
                   {post.data.date && (
                     <p className="uppercase mb-3 font-bold opacity-60">
                       {post.data.date}
@@ -40,22 +35,11 @@ export default function Index({ posts, globalData }) {
                     </p>
                   )}
                   <ArrowIcon className="mt-4" />
-                </a>
               </Link>
             </li>
           ))}
         </ul>
-      </main>
-      <Footer copyrightText={globalData.footerText} />
-      <GradientBackground
-        variant="large"
-        className="fixed top-20 opacity-40 dark:opacity-60"
-      />
-      <GradientBackground
-        variant="small"
-        className="absolute bottom-0 opacity-20 dark:opacity-10"
-      />
-    </Layout>
+    </>
   );
 }
 
