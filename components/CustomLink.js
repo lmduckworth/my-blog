9905286ea117bf3835288a2e29958ed9242efd38
Lee <71070167+lmduckworth@users.noreply.github.com>
2023-01-06 12:05:59 +0000

    Fix issue with build due to missing space between link and a

--- components/CustomLink.js
+++ components/CustomLink.js
@@ -3,7 +3,9 @@ import Link from 'next/link';
 export default function CustomLink({ as, href, ...otherProps }) {
   return (
     <>
-      <Link as={as} href={href}>{...otherProps}</Link>
+      <Link as={as} href={href}>
+        <a {...otherProps} />
+      </Link>
     </>
   );
 }
